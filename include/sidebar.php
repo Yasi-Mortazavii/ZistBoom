62d89aea7b7844c025ec7ecfb53e1ff21ac8289b
Yasi Mortazavii <Mortazaviiyasi@gmail.com>
2022-07-02 15:39:41 +0430

    init Zist Boom with php7

---
+++ include/sidebar.php
@@ -0,0 +1,87 @@
+<?php
+$query_categories = "SELECT * FROM categories";
+
+$categories = $db->query($query_categories);
+
+?>
+<div class="col-md-4 mb-3">
+
+    <div class="card bg-light mb-3">
+        <div class="card-body">
+            <h5 class="card-title">جستجو در وبلاگ</h5>
+            <form action="search.php" method="get">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend order-2">
+                        <button class="btn btn-outline-primary" type="submit">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                    <input name="search" type="text" class="form-control" placeholder="جستجو ...">
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="list-group mb-3">
+        <a href="#" class="list-group-item list-group-item-action active">
+            دسته بندی ها
+        </a>
+        <?php
+        if ($categories->rowCount() > 0) {
+            foreach ($categories as $category) {
+                ?>
+                <a href="index.php?category=<?php echo $category['id'] ?>" class="list-group-item list-group-item-action">
+                    <?php echo $category['title'] ?>
+                </a>
+        <?php
+            }
+        }
+
+        ?>
+
+    </div>
+
+
+    <div class="card bg-light mb-3 p-3">
+        <div class="card-body">
+            <?php
+            if (isset($_POST['subscribe'])) {
+                if (trim($_POST['name']) != "" || trim($_POST['email']) != "") {
+
+                    $name = $_POST['name'];
+                    $email = $_POST['email'];
+
+                    $subscribe_insert = $db->prepare("INSERT INTO subscribers (name, email) VALUES (:name , :email)");
+                    $subscribe_insert->execute(['name' => $name, 'email' => $email]);
+
+                } else {
+                    echo "فیلد ها نباید خالی باشند";
+                }
+            }
+            ?>
+            <form method="post">
+                <div class="form-group">
+                    <label for="name">نام</label>
+                    <input type="text" name="name" id="name" class="form-control" placeholder="نام خود را وارد کنید.">
+
+                </div>
+                <div class="form-group">
+                    <label for="email">ایمیل</label>
+                    <input type="email" name="email" class="form-control" id="email"placeholder="ایمیل خود را وارد کنید.">
+                </div>
+
+                <button type="submit" name="subscribe" class="btn btn-outline-primary btn-block">ارسال</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="card p-3">
+        <div class="card-body">
+            <h3> درباره ما </h2>
+                <p class="text-justify">
+                زیست‌بوم یا زیمانبه مناطق زندگی تمام گیاهان، جانوران و سایر موجودات با شرایط طبیعی محیط در یک منطقه ویژه گفته می‌شود. یک زیمان با انواع حیات جانوری مشخص با توجه به موقعیت، شرایط آب وهوایی، عرض جغرافیایی و ارتفاع تعیین می‌شود.
+ما در زیست بوم با زیست‌بوم‌های اصلی کره زمین اشنا می شیم و اطلاعاتمون را با هم به اشتراک می گذاریم.        </div>
+    </div>
+
+</div>
